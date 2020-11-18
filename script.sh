708934b40bb30cd9f3aeb9b8d13faddbe766826b
mohantymona <48739941+mohantymona@users.noreply.github.com>
2020-11-18 18:46:32 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ $(git log -n1 --since="2 minutes ago") ]]; then
+  BUILD_OUTPUT=$(basename "$PWD")
+fi
+echo $BUILD_OUTPUT
+cd $CODEBUILD_SRC_DIR_Source2
+if [[ $(git log -n1 --since="2 minutes ago") ]]; then
+  BUILD_OUTPUT=$(basename "$PWD")
+fi
+echo $BUILD_OUTPUT
