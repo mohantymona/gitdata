5bb460a4964c5ee035a61e2d3ae75998a4f55294
mohantymona <48739941+mohantymona@users.noreply.github.com>
2020-11-18 18:52:44 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 if [[ $(git log -n1 --since="2 minutes ago") ]]; then
   BUILD_OUTPUT=$(basename "$PWD")
 fi
-echo $BUILD_OUTPUT
 cd $CODEBUILD_SRC_DIR_Source2
 if [[ $(git log -n1 --since="2 minutes ago") ]]; then
   BUILD_OUTPUT=$(basename "$PWD")
 fi
 echo $BUILD_OUTPUT
+
